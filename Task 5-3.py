ece7a05af30da4f816c21d90b54799441c2b53ab
Ruban-swiss <150284154+Ruban-swiss@users.noreply.github.com>
2023-11-08 19:11:25 +0100

    add Task 5-3

---
+++ Task 5-3.py
@@ -0,0 +1,10 @@
+string = input ("Etner the string with vowels:" )
+
+vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+result = ""
+
+for i in range(len(string)):
+    if string[i] not in vowels:
+        result = result + string[i]
+
+print("After removing Vowels: ", result)
